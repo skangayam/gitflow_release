3da5f78eebf831453649fe98ed13afd0f3a00bb0
Shashi Kangayam <Shashi_Kangayam@intuit.com>
2016-09-22 22:14:15 -0700

    added classes

---
+++ modules/module_a/src/main/java/com/skangayam/gitflow_release/Foo.java
@@ -0,0 +1,10 @@
+package com.skangayam.gitflow_release;
+
+public class Foo {
+    public static void main(String[] args){
+        Foo obj = new Foo();
+        System.out.println("Hello from "+ obj.getClass().getName());
+    }
+
+}
+
