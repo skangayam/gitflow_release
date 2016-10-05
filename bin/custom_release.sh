377d5383392a551b295ca59e1d2eb453ba49403b
Shashi Kangayam <Shashi_Kangayam@intuit.com>
2016-10-04 18:26:40 -0700

    g

---
+++ bin/custom_release.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+usage() {
+  echo "usage: " `basename ${0}` "old-version new-version"
+  exit 1
+}
+
+if [[ $# -ne 1 ]] ; then
+  usage
+else
+    oldVersion=$1
+    newVersion=$2
+    git checkout -b release/${newVersion} develop
+    sed -i '' "s/${oldVersion}-SNAPSHOT/${newVersion}/g" ../pom.xml
+    sed -i '' "s/${oldVersion}-SNAPSHOT/${newVersion}/g" ../modules/module_a/pom.xml
+    git add .
+    git commit -m "updated version to ${newVersion}"
+#    git push -u origin release/${newVersion}
+
+    git checkout master
+    git merge release/${newVersion} -X theirs
+    git push -u origin master
+
+    git checkout release/${newVersion}
+    sed -i '' "s/${newVersion}/${newVersion}-SNAPSHOT/g" ../pom.xml
+    sed -i '' "s/${newVersion}/${newVersion}-SNAPSHOT/g" ../modules/module_a/pom.xml
+    git add .
+    git commit -m "added -SNAPSHOT to version"
+
+    git checkout develop
+    git merge release/${newVersion} -X theirs
+    git push -u origin develop
+
+    git branch -D release/${newVersion}
+fi
