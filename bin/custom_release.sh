3b789ec54d5f73b80edc8cb448ea8e2c1f45a060
Shashi Kangayam <Shashi_Kangayam@intuit.com>
2016-10-04 18:35:41 -0700

    u

--- bin/custom_release.sh
+++ bin/custom_release.sh
@@ -11,8 +11,8 @@ else
     oldVersion=$1
     newVersion=$2
     git checkout -b release/${newVersion} develop
-    sed -i '' "s/${oldVersion}-SNAPSHOT/${newVersion}/g" ../pom.xml
-    sed -i '' "s/${oldVersion}-SNAPSHOT/${newVersion}/g" ../modules/module_a/pom.xml
+    sed -i '' "s/${oldVersion}-SNAPSHOT/${newVersion}/g" pom.xml
+    sed -i '' "s/${oldVersion}-SNAPSHOT/${newVersion}/g" modules/module_a/pom.xml
     git add .
     git commit -m "updated version to ${newVersion}"
 #    git push -u origin release/${newVersion}
@@ -22,8 +22,8 @@ else
     git push -u origin master
 
     git checkout release/${newVersion}
-    sed -i '' "s/${newVersion}/${newVersion}-SNAPSHOT/g" ../pom.xml
-    sed -i '' "s/${newVersion}/${newVersion}-SNAPSHOT/g" ../modules/module_a/pom.xml
+    sed -i '' "s/${newVersion}/${newVersion}-SNAPSHOT/g" pom.xml
+    sed -i '' "s/${newVersion}/${newVersion}-SNAPSHOT/g" modules/module_a/pom.xml
     git add .
     git commit -m "added -SNAPSHOT to version"
 
